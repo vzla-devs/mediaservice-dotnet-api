260f010dcbb4a810fbcee1ea3d68dc91f4ac0ebb
Gabriel Nuñez <gabriel.nzn@gmail.com>
2021-06-13 16:43:54 +0100

    adding controller

---
+++ VehiclesDealerApi/Controllers/PicturesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+
+namespace VehiclesDealerApi.Controllers
+{
+    [Route("api/pictures")]
+    [ApiController]
+    public class PicturesController : ControllerBase {
+        [HttpGet]
+        public OkResult Get() {
+            return Ok();
+        }
+
+        [HttpPost]
+        public OkResult Post([FromBody] List<IFormFile> pictures) {
+            pictures.ForEach(async pic => {
+                var filePath = Path.GetFullPath("public/uploads");
+                var fileName = Path.GetTempFileName();
+                var fullPath = Path.Combine(filePath, fileName);
+                using FileStream stream = System.IO.File.Create(fullPath);
+                await pic.CopyToAsync(stream);
+            });
+            return Ok();
+        }
+    }
+}
